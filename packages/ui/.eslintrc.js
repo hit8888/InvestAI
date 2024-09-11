500bc087703118c678b933fa22f7ac9f299ac5ee
SankhaChak <anonproductionspro@gmail.com>
2024-09-11 13:14:38 +0530

    chore: ⚓ rename scope

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
+  extends: ["@meaku/eslint-config/react.js"],
 };
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,5 +1,5 @@
+import sharedConfig from "@meaku/tailwind-config";
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
 
 const config: Pick<Config, "prefix" | "presets" | "content"> = {
   content: ["./src/**/*.tsx"],
