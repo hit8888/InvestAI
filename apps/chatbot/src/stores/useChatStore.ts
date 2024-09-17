7f0a642c50933d55402b271e3f2ca1a1fa07e4a0
SankhaChak <anonproductionspro@gmail.com>
2024-09-17 20:22:40 +0530

    feat: ✅ define stores

--- apps/chatbot/src/stores/useChatStore.ts
+++ apps/chatbot/src/stores/useChatStore.ts
@@ -1,3 +1,4 @@
+import { Configuration, Session } from "@meaku/core/types/session";
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
@@ -9,6 +10,12 @@ interface State {
   setOrgName: (orgName: string) => void;
   agentId: string | null;
   setAgentId: (agentId: string) => void;
+  session: Session | null;
+  setSession: (session: Session) => void;
+  configuration: Configuration | null;
+  setConfiguration: (configuration: Configuration) => void;
+  hasFirstUserMessageBeenSent: boolean;
+  setHasFirstUserMessageBeenSent: (value: boolean) => void;
 }
 
 export const useChatStore = create<State>()(
@@ -29,6 +36,21 @@ export const useChatStore = create<State>()(
       setAgentId: (agentId) =>
         set((draft) => {
           draft.agentId = agentId;
+        }),
+      session: null,
+      setSession: (session) =>
+        set((draft) => {
+          draft.session = session;
+        }),
+      configuration: null,
+      setConfiguration: (configuration) =>
+        set((draft) => {
+          draft.configuration = configuration;
+        }),
+      hasFirstUserMessageBeenSent: false,
+      setHasFirstUserMessageBeenSent: (value) =>
+        set((draft) => {
+          draft.hasFirstUserMessageBeenSent = value;
         }),
     })),
   ),
--- apps/chatbot/src/stores/useMessageStore.ts
+++ apps/chatbot/src/stores/useMessageStore.ts
@@ -1,18 +1,90 @@
-import { ChatConfig } from "@meaku/core/types/config";
+import { AIResponse, Message } from "@meaku/core/types/chat";
+import { nanoid } from "nanoid";
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 interface State {
-  config: ChatConfig | null;
-  setConfig: (config: ChatConfig) => void;
+  messages: Message[];
+  setMessages: (messages: Message[], welcomeMessage: string) => void;
+  suggestedQuestions: string[];
+  setSuggestedQuestions: (suggestedQuestions: string[]) => void;
+  isAMessageBeingProcessed: boolean;
+  setIsAMessageBeingProcessed: (isAMessageBeingProcessed: boolean) => void;
+  handleAddAIMessage: (response: AIResponse) => void;
+  handleAddUserMessage: (message: string) => void;
 }
 
 export const useMessageStore = create<State>()(
   devtools(
     immer((set) => ({
-      config: null,
-      setConfig: (config) => set((state) => (state.config = config)),
+      messages: [],
+      setMessages: (messages, welcomeMessage) =>
+        set((draft) => {
+          let updatedMessages = [...messages];
+
+          updatedMessages.splice(0, 0, {
+            role: "ai",
+            message: welcomeMessage,
+            media: null,
+            documents: [],
+            id: nanoid(),
+          });
+
+          updatedMessages = updatedMessages.map((message) => ({
+            ...message,
+            isPartOfHistory: true,
+          }));
+
+          draft.messages = updatedMessages;
+        }),
+      suggestedQuestions: [],
+      setSuggestedQuestions: (suggestedQuestions) =>
+        set((draft) => {
+          draft.suggestedQuestions = suggestedQuestions;
+        }),
+      isAMessageBeingProcessed: false,
+      setIsAMessageBeingProcessed: (isAMessageBeingProcessed) =>
+        set((draft) => {
+          draft.isAMessageBeingProcessed = isAMessageBeingProcessed;
+        }),
+      handleAddAIMessage: (response) =>
+        set((draft) => {
+          const messageId = response.response_id;
+
+          const existingMessageIndex = draft.messages.findIndex(
+            (message) => message.id === messageId,
+          );
+
+          if (existingMessageIndex !== -1) {
+            draft.messages[existingMessageIndex] = {
+              ...draft.messages[existingMessageIndex],
+              message: response.message,
+              media: response.media,
+              documents: response.documents,
+              is_loading: response.is_loading,
+            };
+          } else {
+            draft.messages.push({
+              id: messageId,
+              role: "ai",
+              message: response.message,
+              media: response.media,
+              documents: response.documents,
+              is_loading: response.is_loading,
+            });
+          }
+        }),
+      handleAddUserMessage: (message) =>
+        set((draft) => {
+          draft.messages.push({
+            id: nanoid(),
+            role: "user",
+            message,
+            media: null,
+            documents: [],
+          });
+        }),
     })),
   ),
 );
