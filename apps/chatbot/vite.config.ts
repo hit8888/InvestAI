ad1bf366a6bb22f20f43f2dc41e68d6fbaef7bcc
SankhaChak <anonproductionspro@gmail.com>
2024-09-27 16:11:56 +0530

    hotfix: 🐞 add sentry config

--- apps/chatbot/vite.config.ts
+++ apps/chatbot/vite.config.ts
@@ -7,14 +7,14 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// TODO: Replace the org and project name with the one that is present in org Sentry
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     sentryVitePlugin({
-      org: "acme-30",
-      project: "javascript-react",
+      org: "breakout",
+      project: "react-frontend/",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
       sourcemaps: {
         assets: "./dist/**",
         filesToDeleteAfterUpload: ["./dist/**/*.map"],
