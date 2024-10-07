43456eeb93e21a58ce33ee737570e9f7e8f0304b
SankhaChak <anonproductionspro@gmail.com>
2024-10-07 16:37:09 +0530

    feat: ✅ add logrocket

--- apps/chatbot/src/App.tsx
+++ apps/chatbot/src/App.tsx
@@ -1,8 +1,10 @@
 import * as amplitude from "@amplitude/analytics-browser";
+import LogRocket from "logrocket";
 import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { ENV } from "./config/env";
 import router from "./router";
+import { trackError } from "./utils/error";
 
 function App() {
   useEffect(() => {
@@ -10,7 +12,23 @@ function App() {
       amplitude.init(ENV.VITE_AMPLITUDE_API_KEY, {
         autocapture: true,
       });
-    } catch (error) {}
+    } catch (error) {
+      trackError(error, {
+        action: "initialize_amplitude",
+        component: "App",
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    try {
+      LogRocket.init(ENV.VITE_LOGROCKET_APP_ID);
+    } catch (error) {
+      trackError(error, {
+        action: "initialize_logrocket",
+        component: "App",
+      });
+    }
   }, []);
 
   return (
