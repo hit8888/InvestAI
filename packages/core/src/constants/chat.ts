6ac64375fc12c9bbef931a89f965345ca92db2fb
SankhaChak <anonproductionspro@gmail.com>
2024-10-11 21:19:32 +0530

    fix: 🐞 dynamic processing message text

--- packages/core/src/constants/chat.ts
+++ packages/core/src/constants/chat.ts
@@ -1,7 +1,1 @@
-export const PROCESSING_MESSAGE_SEQUENCE = [
-  "Sam is thinking...",
-  "Hang tight, Sam is working on it...",
-  "Just a few more seconds...",
-  "Almost there, Sam is getting everything in place...",
-  "Sam is almost done...",
-];
+export {};
--- packages/ui/src/utils/chat.ts
+++ packages/ui/src/utils/chat.ts
@@ -1,8 +1,1 @@
-import { ChatConfig } from "@meaku/core/types/config";
-
-export const getChatHeaderText = (config: ChatConfig, orgName: string) => {
-  if (config === ChatConfig.EMBED)
-    return `You’re now talking to Sam, our Smart Agent at ${orgName}.`;
-
-  return `Need help navigating ${orgName}? Our AI assistant is here to answer your questions.`;
-};
+export {};
