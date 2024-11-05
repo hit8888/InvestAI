be11da4359b52509d2274ab48cfef6f7987ed6e4
Krishna Kant Singh <krishnakantsingh@192.168.1.2>
2024-11-05 21:31:20 +0530

    Add chnages

--- packages/design-system/src/components/Orb/index.tsx
+++ packages/design-system/src/components/Orb/index.tsx
@@ -21,9 +21,9 @@ interface IProps {
 
 const Orb = ({ color, state }: IProps) => {
     return (
-        <div className={cn("h-12 w-12 flex justify-center orb-container rounded-custom-56 shining p-1", {
-            "animate-taking-input": state === OrbStatusEnum.takingInput,
-            "animate-thinking": state === OrbStatusEnum.thinking,
+        <div className={cn("h-12 w-12 flex justify-center  rounded-custom-56 shining p-1", {
+            "animate-taking-input orb-container": state === OrbStatusEnum.takingInput,
+            "animate-thinking orb-container": state === OrbStatusEnum.thinking,
             "animate-impatient": state === OrbStatusEnum.impatient,
         })} style={{ "--input-color": color } as React.CSSProperties}>
             <ShiningRectangle width="33" height="17" />
