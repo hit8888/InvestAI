8d9d877bde4b00eb568688c37dd0d7b2115f0861
SankhaChak <anonproductionspro@gmail.com>
2024-11-12 15:16:42 +0530

    fix: 🐞 slide responsiveness [re]

--- apps/chatbot/src/components/views/MultimediaChat/SlideItem.tsx
+++ apps/chatbot/src/components/views/MultimediaChat/SlideItem.tsx
@@ -10,7 +10,7 @@ const SlideItem = (props: IProps) => {
   const { icon, title } = props;
 
   return (
-    <div className="flex flex-col items-center gap-7 rounded-xl border border-primary/25 bg-white p-4 text-center shadow-xl">
+    <div className="flex h-full w-full flex-col items-center gap-7 rounded-xl border border-primary/25 bg-white p-4 text-center shadow-xl">
       <div className="flex h-16 w-full items-center justify-center rounded-xl border border-primary/10 bg-primary/5">
         <DynamicIcon icon={icon} className="h-10 w-10 text-primary" />
       </div>
