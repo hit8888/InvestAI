e14c9c0d2cf901125f59e3997006848c4382b421
Krishna Kant Singh <krishnakantsingh@192.168.1.3>
2024-11-18 08:52:48 +0530

    Add changes

--- apps/chatbot/src/stores/useArtifactStore.ts
+++ apps/chatbot/src/stores/useArtifactStore.ts
@@ -1,36 +1,17 @@
-import { ArtifactEnum } from '@meaku/core/types/chat';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
 interface State {
-  activeArtifactId: string | null;
-  setActiveArtifactId: (artifactId: string | null) => void;
-  activeArtifactType: ArtifactEnum | null;
-  setActiveArtifactType: (artifactType: ArtifactEnum | null) => void;
   isArtifactPlaying: boolean;
   setIsArtifactPlaying: (isPlaying: boolean) => void;
   shouldEndArtifactImmediately: boolean;
   setShouldEndArtifactImmediately: (shouldEnd: boolean) => void;
-  handleRemoveActiveArtifact: () => void;
-  handleAddActiveArtifact: (artifactId: string, artifactType: ArtifactEnum) => void;
 }
 //TODO: Remove useArtifactStore;
 export const useArtifactStore = create<State>()(
   devtools(
     immer((set) => ({
-      activeArtifactId: null,
-      setActiveArtifactId: (artifactId) => {
-        set((state) => {
-          state.activeArtifactId = artifactId;
-        });
-      },
-      activeArtifactType: null,
-      setActiveArtifactType: (artifactType) => {
-        set((state) => {
-          state.activeArtifactType = artifactType;
-        });
-      },
       isArtifactPlaying: false,
       setIsArtifactPlaying: (isPlaying) => {
         set((state) => {
@@ -41,18 +22,6 @@ export const useArtifactStore = create<State>()(
       setShouldEndArtifactImmediately: (shouldEnd) => {
         set((state) => {
           state.shouldEndArtifactImmediately = shouldEnd;
-        });
-      },
-      handleRemoveActiveArtifact: () => {
-        set((state) => {
-          state.activeArtifactId = null;
-          state.activeArtifactType = null;
-        });
-      },
-      handleAddActiveArtifact: (artifactId, artifactType) => {
-        set((state) => {
-          state.activeArtifactId = artifactId;
-          state.activeArtifactType = artifactType;
         });
       },
     })),
