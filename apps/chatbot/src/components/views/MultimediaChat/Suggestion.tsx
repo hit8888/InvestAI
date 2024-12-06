3a5bd133f95a4b6ce1f8a50a240c98e61f94129e
Krishna Kant Singh <krishnakantsingh@192.168.1.2>
2024-12-06 17:17:55 +0530

    Add changes

--- apps/chatbot/src/components/views/MultimediaChat/Suggestion.tsx
+++ apps/chatbot/src/components/views/MultimediaChat/Suggestion.tsx
@@ -10,7 +10,7 @@ const Suggestion = ({ question, onSuggestedQuestionOnClick }: IProps) => {
     <button
       type="button"
       onClick={() => onSuggestedQuestionOnClick(question)}
-      className="group flex items-center justify-center gap-1 rounded-full border-2 border-primary/10 bg-primary-50 p-2 text-primary transition-all duration-300 ease-in-out hover:bg-primary-300"
+      className="group flex items-center justify-center gap-1 rounded-full border-2 border-primary/10 bg-primary-50 p-2 text-primary transition-all duration-300 ease-in-out hover:bg-primary/40"
     >
       <SparkleIcon className="!h-4 !w-4 fill-primary-400 transition-colors duration-300 ease-in-out group-hover:fill-white/60" />
       <span className="min-w-max text-sm font-medium">{question}</span>
