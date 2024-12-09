df545da11f371f43756417b938a55fae63c781cc
Krishna Kant Singh <krishnakantsingh@Krishna-ka-MacBook-Pro.local>
2024-12-09 15:30:14 +0530

    Fix basic styling

--- packages/design-system/src/components/Orb/index.stories.tsx
+++ packages/design-system/src/components/Orb/index.stories.tsx
@@ -2,37 +2,39 @@ import { Meta } from '@storybook/react';
 import Orb, { OrbStatusEnum } from './index';
 
 export default {
-    title: 'Components/Orb',
-    component: Orb,
-    argTypes: {
-        color: { control: 'color' },
-    },
+  title: 'Components/Orb',
+  component: Orb,
+  argTypes: {
+    color: { control: 'color' },
+  },
 } as Meta;
 
-
-
-const Template = (args) => <div className='flex justify-center items-center'><Orb {...args} /></div>;
+const Template = (args) => (
+  <div className="flex items-center justify-center">
+    <Orb {...args} />
+  </div>
+);
 
 export const TakingInput = Template.bind({});
 TakingInput.args = {
-    color: '#acb2eb',
-    state: OrbStatusEnum.takingInput,
+  color: '#acb2eb',
+  state: OrbStatusEnum.takingInput,
 };
 
 export const Thinking = Template.bind({});
 Thinking.args = {
-    color: '#acb2eb',
-    state: OrbStatusEnum.thinking,
+  color: '#acb2eb',
+  state: OrbStatusEnum.thinking,
 };
 
 export const Responding = Template.bind({});
 Responding.args = {
-    color: '#acb2eb',
-    state: OrbStatusEnum.responding,
+  color: '#acb2eb',
+  state: OrbStatusEnum.responding,
 };
 
-export const Impatient = Template.bind({});
-Impatient.args = {
-    color: '#acb2eb',
-    state: OrbStatusEnum.impatient,
+export const Waiting = Template.bind({});
+Waiting.args = {
+  color: '#acb2eb',
+  state: OrbStatusEnum.waiting,
 };
--- packages/design-system/src/components/Orb/index.tsx
+++ packages/design-system/src/components/Orb/index.tsx
@@ -1,34 +1,33 @@
-
-
-import { cn } from "../../lib/cn";
-import ShiningRectangle from "../icons/ShiningRectangle";
-import "./index.css"
-
+import { cn } from '../../lib/cn';
+import ShiningRectangle from '../icons/ShiningRectangle';
+import './index.css';
 
 export enum OrbStatusEnum {
-    takingInput = 'takingInput',
-    thinking = 'thinking',
-    responding = 'responding',
-    impatient = 'impatient',
+  takingInput = 'takingInput',
+  thinking = 'thinking',
+  responding = 'responding',
+  waiting = 'waiting',
 }
-
 
 interface IProps {
-    color: string;
-    state: OrbStatusEnum;
+  color: string;
+  state: OrbStatusEnum;
 }
-
 
 const Orb = ({ color, state }: IProps) => {
-    return (
-        <div className={cn("h-12 w-12 flex justify-center  rounded-custom-56 shining p-1", {
-            "animate-taking-input orb-container": state === OrbStatusEnum.takingInput,
-            "animate-thinking orb-container": state === OrbStatusEnum.thinking,
-            "animate-impatient": state === OrbStatusEnum.impatient,
-        })} style={{ "--input-color": color } as React.CSSProperties}>
-            <ShiningRectangle width="33" height="17" />
-        </div>
-    )
-}
+  return (
+    <div
+      className={cn('shining flex h-12 w-12 justify-center rounded-custom-56 p-1', {
+        'animate-taking-input orb-container': state === OrbStatusEnum.takingInput,
+        'animate-thinking orb-container': state === OrbStatusEnum.thinking,
+        'animate-waiting': state === OrbStatusEnum.waiting,
+        'animate-responding': state === OrbStatusEnum.responding,
+      })}
+      style={{ '--input-color': color, '--fallback-color': '#acb2eb' } as React.CSSProperties}
+    >
+      <ShiningRectangle width="33" height="17" />
+    </div>
+  );
+};
 
 export default Orb;
