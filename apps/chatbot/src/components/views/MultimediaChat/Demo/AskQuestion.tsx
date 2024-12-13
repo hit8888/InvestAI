2ff44a4a009521710e0f9d21f35f4c2010db782e
Krishna Kant Singh <krishnakantsingh@Krishna-ka-MacBook-Pro.local>
2024-12-13 14:51:33 +0530

    Fix code

---
+++ apps/chatbot/src/components/views/MultimediaChat/Demo/AskQuestion.tsx
@@ -0,0 +1,11 @@
+import RaiseHand from '@breakout/design-system/components/icons/RaiseHand';
+
+const AskQuestion = () => {
+  return (
+    <div className="flex items-center gap-2 px-4 py-1">
+      <RaiseHand height={24} width={24} color="rgba(var(--primary), 0.9)" />
+    </div>
+  );
+};
+
+export default AskQuestion;
