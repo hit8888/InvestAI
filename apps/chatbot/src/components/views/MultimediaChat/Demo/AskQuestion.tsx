0a8bb7dc50cd7b4f43f3ef0cdcf840ae39408fe8
Krishna Kant Singh <krishnakantsingh@Krishna-ka-MacBook-Pro.local>
2024-12-13 16:06:23 +0530

    Cursor pointer

--- apps/chatbot/src/components/views/MultimediaChat/Demo/AskQuestion.tsx
+++ apps/chatbot/src/components/views/MultimediaChat/Demo/AskQuestion.tsx
@@ -2,7 +2,8 @@ import RaiseHand from '@breakout/design-system/components/icons/RaiseHand';
 
 const AskQuestion = () => {
   return (
-    <div className="flex items-center gap-2 px-4 py-1">
+    <div className="flex cursor-pointer items-center gap-2 rounded-[50px] px-4 py-1">
+      <span className="text-sm text-primary">Ask Question</span>
       <RaiseHand height={24} width={24} color="rgba(var(--primary), 0.9)" />
     </div>
   );
