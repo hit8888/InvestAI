0ef85de9cbe798b69b7fbcdfdbde616e764ed259
Krishna Kant Singh <krishnakantsingh@Krishna-ka-MacBook-Pro.local>
2024-12-18 10:34:54 +0530

    Fix code

--- apps/chatbot/src/utils/common.ts
+++ apps/chatbot/src/utils/common.ts
@@ -31,10 +31,10 @@ export const capitalizeString = (string: string) => {
 
 export const getProcessingMessageSequence = () => {
   return [
-    `Thinking...`,
-    `Hang tight, working on it...`,
-    'Just a few more seconds...',
-    `Almost there, getting everything in place...`,
-    `Almost done...`,
+    `Sam is thinking...`,
+    `Putting together my answer..`,
+    `Getting it ready..`,
+    `Working on it..`,
+    `Forming a complete response..`,
   ];
 };
