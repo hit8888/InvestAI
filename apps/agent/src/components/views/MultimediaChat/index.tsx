51f3f6a73a4133fc033e04bb01d89db2298eab53
krishnaBreakout <krishna@getbreakout.ai>
2025-01-08 14:30:16 +0530

    BO-0000: Fix commit (#163)
    Fix commit for Homepage background
    
    Co-authored-by: Krishna Kant Singh <krishnakantsingh@Krishna-ka-MacBook-Pro.local>

--- apps/agent/src/components/views/MultimediaChat/index.tsx
+++ apps/agent/src/components/views/MultimediaChat/index.tsx
@@ -1,6 +1,5 @@
 import { cn } from '@breakout/design-system/lib/cn';
 import { memo } from 'react';
-import { useParams } from 'react-router-dom';
 
 import EntryPointBottomBar from './EntryPointBottomBar.tsx';
 import useAgentbotAnalytics from '../../../hooks/useAgentbotAnalytics.tsx';
@@ -10,7 +9,6 @@ import { useEmbedAppEvents } from '../../../hooks/useEmbedAppEvents.ts';
 import { useMessageStore } from '../../../stores/useMessageStore.ts';
 import { useUrlParams } from '../../../hooks/useUrlParams.ts';
 import AgentInOpenState from './AgentInOpenState.tsx';
-import { CDN_URL_FOR_ASSETS } from '../../../constants/chat.ts';
 
 interface IProps {
   fetchSessionData: () => void;
@@ -20,9 +18,6 @@ const Multimedia = ({ fetchSessionData }: IProps) => {
   const { handleSendUserMessage } = useWebSocketChat();
   const { getParam, setParam } = useUrlParams();
   const isAgentOpen = getParam('isAgentOpen') === 'true';
-  const isShowingBGCover = getParam('showBackgroundCover') === 'true';
-
-  const { orgName } = useParams<{ orgName: string }>();
 
   const hasFirstUserMessageBeenSent = useMessageStore((state) => state.hasFirstUserMessageBeenSent);
   const handleSendMessage = (data: IWebSocketHandleMessage) => {
@@ -55,11 +50,7 @@ const Multimedia = ({ fetchSessionData }: IProps) => {
     <div
       className={cn('mx-auto mt-2 flex h-[95vh] w-[98vw] justify-center font-inter', {
         'rounded-2xl': isAgentOpen,
-        'rounded-2xl bg-gray-300 bg-contain': isShowingBGCover,
       })}
-      style={{
-        backgroundImage: isShowingBGCover ? `url(${CDN_URL_FOR_ASSETS}${orgName}.png)` : undefined,
-      }}
     >
       {isAgentOpen ? (
         <AgentInOpenState handleSendMessage={handleSendMessage} handleCloseAgent={handleCloseAgent} />
