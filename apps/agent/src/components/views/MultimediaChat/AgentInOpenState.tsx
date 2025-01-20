6dfea392f048b484126c651c465332e86041de45
Krishna Kant Singh <krishnakantsingh@Krishna-ka-MacBook-Pro.local>
2025-01-20 17:18:50 +0530

    FIx code

--- apps/agent/src/components/views/MultimediaChat/AgentInOpenState.tsx
+++ apps/agent/src/components/views/MultimediaChat/AgentInOpenState.tsx
@@ -70,7 +70,7 @@ const AgentInOpenState = ({ handleSendMessage, handleCloseAgent }: IProps) => {
             handleSendMessage={handleSendMessage}
             hasArtifactOrDemoInMessageHistory={hasArtifactOrDemoInMessageHistory}
             isMediaTakingFullWidth={isMediaTakingFullWidth}
-            showDemoPreQuestions={false}
+            showDemoPreQuestions={isDemoAvailable && !demoDetails}
           />
 
           <ArtifactContainer
