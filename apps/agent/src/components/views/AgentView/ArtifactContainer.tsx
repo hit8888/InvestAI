9102efef9739873728802b7e3184323d8f331ab6
meaku-pawan <pawan@getbreakout.ai>
2025-03-03 13:31:24 +0530

    ENG-1451: Removing Artifact loader and slides intact until new one comes (#375)
    removing loader and slides intact
    
    Co-authored-by: Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>

--- apps/agent/src/components/views/AgentView/ArtifactContainer.tsx
+++ apps/agent/src/components/views/AgentView/ArtifactContainer.tsx
@@ -4,7 +4,7 @@ import { WebSocketMessage, ArtifactMessageContent } from '@meaku/core/types/webS
 import { useMessageStore } from '../../../stores/useMessageStore';
 import { ArtifactContent } from '@meaku/core/types/artifact';
 import { isArtifactMessage, SupportedArtifactType } from '@meaku/core/utils/messageUtils';
-import { useGetArtifactLoadingState } from '../../../hooks/useGetArtifactLoadingState';
+// import { useGetArtifactLoadingState } from '../../../hooks/useGetArtifactLoadingState';
 
 type IProps = {
   logoURL: string;
@@ -19,7 +19,7 @@ const ArtifactContainer = ({ logoURL, isMediaTakingFullWidth, handleSendMessage,
   const messages = useMessageStore((state) => state.messages);
   const activeArtifact = useArtifactStore((state) => state.activeArtifact);
 
-  const { hasGeneratingArtifactEvents } = useGetArtifactLoadingState();
+  // const { hasGeneratingArtifactEvents } = useGetArtifactLoadingState();
 
   // Find the message that corresponds to the active artifact
   const artifactMessage = activeArtifact
@@ -51,7 +51,7 @@ const ArtifactContainer = ({ logoURL, isMediaTakingFullWidth, handleSendMessage,
       setIsArtifactPlaying={setIsArtifactPlaying}
       activeArtifact={artifactWithContent}
       onSlideItemClick={onSlideItemClick}
-      isGeneratingArtifact={hasGeneratingArtifactEvents}
+      isGeneratingArtifact={false} // TODO: keeping it false for now, there is Too much gap b/w slides ( Linear task:- ENG-1451)
       artifactContent={artifactContent}
     />
   );
