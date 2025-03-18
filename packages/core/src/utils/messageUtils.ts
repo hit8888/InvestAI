feffd453b40405a78c537e1ef9e114ba76ea7bea
Amogh Jalan <34140672+amoghjalan@users.noreply.github.com>
2025-03-18 16:02:19 +0530

    ENG-1303 User Inactivity Messages (#420)
    * User Inactivity Messages
    
    * User Inactivity Messages
    
    * Fix inactivity messages

--- packages/core/src/utils/messageUtils.ts
+++ packages/core/src/utils/messageUtils.ts
@@ -164,7 +164,8 @@ export const checkIsDiscoveryMessage = (message: WebSocketMessage): boolean => {
 export const checkIsMainResponseMessage = (message: WebSocketMessage): boolean => {
   return (
     (message.actor === 'SALES' && (isStreamMessage(message) || isTextMessage(message))) ||
-    (message.actor === 'ARTIFACT' && isTextMessage(message)) //Being used for form acknowledgement
+    (message.actor === 'ARTIFACT' && isTextMessage(message)) || //Being used for form acknowledgement
+    (message.actor === 'EVENT' && (isStreamMessage(message) || isTextMessage(message)))
   );
 };
 
